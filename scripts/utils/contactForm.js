adecc0fe362dfdc35df1fcf5ef5f1218017e461f
Lord Shaxx <87854378+Korootak@users.noreply.github.com>
2022-11-08 22:28:12 +0100

    ajout de regex et verification du form

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -11,14 +11,53 @@ function displayModal() {
   contactModalFormInputs[0].focus();
 }
 
-async function checkFormValidation(fields) {
-  let fieldsValues = Object.values(fields);
-  if (fieldsValues.includes(false) == true) {
-    return false;
+// verifie au moyen d'une regex le mail et retourne sous la forme d'un objet
+// la valeur de la regex est contenue dans le champs value de l'objet
+// le champ id n'est la que pour le nommage
+const verifyEmail = (email, id) => {
+  const emailRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return { value: emailRegex.test(String(email).toLowerCase()), id };
+};
+// verifie au moyen d'une regex le nom et retourne sous la forme d'un objet
+// la valeur de la regex est contenue dans le champs value de l'objet
+// le champ id n'est la que pour le nommage
+const verifyName = (name, id) => {
+  const nameRegex =
+    /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+  return { value: nameRegex.test(String(name).toLowerCase()), id };
+};
+// ceci est une héhésie
+// passe-plât : prend un objet et si la valeur est nulle l'affiche dans la console
+// retourne quoi qu'il se passe l'objet en question
+const display = (obj) => {
+  if (obj.value === false) {
+    console.error(obj.id, obj.value);
   }
-  if (fieldsValues.includes(false) == false) {
-    return true;
+  return obj;
+};
+
+function checkFormValidation(fields) {
+  // parcourt les champs que l'utilisateur remplie et renvoi une erreur si l'un est vide
+  for (let i = 0; i !== fields.length; i++) {
+    const inputValue = fields[i].value;
+    // si pas vide alors saut l'instruction après le block de condition et réitère un nv tour de boucle tant que la condition de sortie est invalide
+    if (inputValue !== "") {
+      continue;
+    }
+    // arrive ici car un des champs est vide
+    throw new Error("one or many fields is/are empty");
   }
+  // recupère les valeurs des champs (value) par déconstruction des objets et renomme la propriété value
+  const { value: firstName } = fields[0];
+  const { value: lastName } = fields[1];
+  const { value: email } = fields[2];
+  // détermine la valeur de retour de la function
+  return (
+    display(verifyEmail(email, "email")).value &&
+    display(verifyName(lastName, "lastName")).value &&
+    display(verifyName(firstName, "fistName")).value
+  );
 }
 
 // Fermeture de la modale
